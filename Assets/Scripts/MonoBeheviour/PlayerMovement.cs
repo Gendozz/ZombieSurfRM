dab03d1b19302cfa300aeca8bca5b8ad5d3826c1
Gennadiy_M <gendozz04@gmail.com>
2022-01-27 11:42:23 +0300

    Before changing Move
    Jump and changing lane working/ Want to remake it more flexible

--- Assets/Scripts/MonoBeheviour/PlayerMovement.cs
+++ Assets/Scripts/MonoBeheviour/PlayerMovement.cs
@@ -6,15 +6,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Settings
-    private float fromLaneToLaneDistance = 3f;
+    private float laneToLaneDistance = 3f;
 
     [SerializeField]
-    private float gravity = 20f;
+    private float currentGravity = 20f;
+
+    [SerializeField]
+    private readonly float normalGravity = 20f;
+    
+    [SerializeField]
+    private readonly float fallingGravity = 60f;
 
     [SerializeField]
     private float jumpForce = 8f;
 
-    private float changeLaneDuration = 0.5f;
+    [SerializeField]
+    private float changeLaneDuration = 0.2f;
+
+    private int maxLanesFromCenter = 1;
 
     // Movements
 
@@ -24,59 +33,132 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 startPosition;
     private Vector3 endPosition;
 
-    private float currentSideDisrection;
+    [SerializeField]
+    private int currentLaneNumber = 0;
+
+    private float currentSideDirection;
+
+    private Vector3 currentMovement = Vector3.zero;
+
+    // Flags
+
+    private bool isChangingLane = false;
 
 
     private CharacterController charController;
 
-    private Vector3 currentMovement;
 
 
 
     private void Start()
     {
         charController = GetComponent<CharacterController>();
+        
+        // Push character controller to floor
+        //currentMovement.y = -1f;
     }
 
     private void Update()
     {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        if(horizontalInput != 0)
-        {
-            StartCoroutine(ChangeLane(horizontalInput));
-            print($"horizontalInput => {horizontalInput}");
-        }
-        verticalInput = Input.GetAxisRaw("Vertical");
-
-        //charController.Move(currentMovement);
+        HandleInput();
+        Move();
 
     }
 
-    IEnumerator ChangeLane(float sideDirection)
+    private void Move()
     {
-        print("ChangeLane started");
-        SetNewXPosition(sideDirection);
+        currentMovement.y -= (currentGravity * Time.deltaTime);
+        charController.Move(currentMovement * Time.deltaTime);
+    }
 
-        float elapsedTime = 0;
-        Vector3 currentPosition = transform.position;
-
-        while(elapsedTime < changeLaneDuration)
+    private void HandleInput()
+    {
+        horizontalInput = 0f;
+        if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            transform.position = Vector3.Lerp(currentPosition, endPosition, (elapsedTime / changeLaneDuration));
-            elapsedTime += Time.deltaTime;
-            yield return null;
+            horizontalInput = -1f;
+        }
+        if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            horizontalInput = 1f;
         }
 
-        //transform.position = endPosition;
+        if (horizontalInput != 0)
+        {
+            bool isLaneInDirection = Mathf.Abs(currentLaneNumber + horizontalInput) <= maxLanesFromCenter;
+
+            if (isLaneInDirection)
+            {
+                StartCoroutine(ChangeLane(horizontalInput));                
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) ||Input.GetKeyDown(KeyCode.Space))
+        {
+            if (charController.isGrounded)
+            {
+                StartCoroutine(Jump());   
+            }           
+        }
+        
+        if(Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            StartCoroutine(Slide());
+        }
+    }
+
+
+    private IEnumerator ChangeLane(float sideDirection)
+    {
+        //for (float waitTime = 0.2f; waitTime > 0.0 && isChangingLane; waitTime -= Time.fixedDeltaTime)
+        //    yield return null;
+
+
+
+        if (!isChangingLane)
+        {
+            isChangingLane = true;
+            currentLaneNumber += (int)sideDirection;
+
+            startPosition = transform.position;
+
+            float newXPosition = transform.position.x + (sideDirection * laneToLaneDistance);
+            float elapsedTime = 0;
+            float currentXPosition;
+
+            while (elapsedTime < changeLaneDuration)
+            {
+                currentXPosition = Mathf.Lerp(startPosition.x, newXPosition, (elapsedTime / changeLaneDuration));
+                transform.position = new Vector3(currentXPosition, transform.position.y, transform.position.z);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            transform.position = new Vector3(newXPosition, transform.position.y, transform.position.z);
+
+            isChangingLane = false;
+        }
+    }
+
+
+
+    IEnumerator Jump()
+    {            
+        currentMovement.y = jumpForce;
+        //print($"Jump coroutine started. curentMovement => {currentMovement} ");
+
+        do
+        {
+            //if(gameObj)
+
+            yield return new WaitForFixedUpdate();
+        } while (!charController.isGrounded);
+        //print($"Jump coroutine ended. curentMovement => {currentMovement} ");
         yield return null;
     }
 
-
-    private void SetNewXPosition(float sideDirection)
+    IEnumerator Slide()
     {
-        startPosition = transform.position;
-        endPosition = transform.position + Vector3.right * sideDirection * fromLaneToLaneDistance;
-        print($"endPosition => {endPosition}");
+        yield return null;
     }
-
 }
